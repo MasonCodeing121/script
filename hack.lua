ad8e75c28f72e494eab03246b0dceb98beca7b91
Pear256 <wavecube05@gmail.com>
2025-11-28 02:22:00 -0600

    Refactor GainMuscle firing logic in hack.lua

--- hack.lua
+++ hack.lua
@@ -1,46 +1,34 @@
 local RepStorage = game:GetService("ReplicatedStorage")
 local RemoteEvent = RepStorage:WaitForChild("RemoteEvent")
 
--- Define the number of times the action should be fired INSIDE a single function call
-local ACTIONS_PER_FIRE_REMOTE = 1000 
+-- Settings for how many times to fire GainMuscle and delay between each
+local ACTIONS_PER_FIRE_REMOTE = 1000
 local DELAY_PER_ACTION = 0.01
 
-local function FireRemote()
-    -- This internal loop fires the 'GainMuscle' remote 1000 times
+local function FireGainMuscle()
+    -- Fires the 'GainMuscle' remote ACTIONS_PER_FIRE_REMOTE times
     for i = 1, ACTIONS_PER_FIRE_REMOTE do
-        -- GainMuscle
         local args = {
             [1] = {
-                [1] = "GainMuscle" -- The action string
+                [1] = "GainMuscle"
             }
         }
-        game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
-
-       
-
-        -- Wait a small delay between each action fire
-        task.wait(DELAY_PER_ACTION) 
+        RemoteEvent:FireServer(unpack(args))
+        task.wait(DELAY_PER_ACTION)
     end
-    print("Completed 1000 'GainMuscle'+'SellMuscle' action pairs within FireRemote.")
+    print("Completed 1000 'GainMuscle' actions inside FireGainMuscle.")
 end
 
--- The variables for the external loop are removed, as the main action is now inside FireRemote
-local numberOfCycles = 100000 -- This number now dictates how many times the 1000-fire-block runs
-local delayBetweenCycles = 0 -- We will let the inner task.wait handle the time
+local numberOfCycles = 100000 -- Repeat blocks of gain muscle
+local delayBetweenCycles = 0 -- Handled by inner wait
 
-print("Starting the FOREVER loop. This will continuously fire 1000 'GainMuscle'+'SellMuscle' pairs per cycle.")
+print("Starting the FOREVER loop. This will continuously fire 1000 'GainMuscle' actions per cycle.")
 
--- The 'while true do' loop runs the code block forever
 while true do
-    print("\n--- Starting new cycle of " .. numberOfCycles .. " blocks of 1000 action pairs ---")
-    
-    -- Inner loop to run the FireRemote function 'numberOfCycles' times
+    print("\n--- Starting new cycle of " .. numberOfCycles .. " blocks of 1000 'GainMuscle' actions ---")
     for cycle = 1, numberOfCycles do
-        -- Calling FireRemote executes 1000 pairs internally
-        FireRemote() 
+        FireGainMuscle()
         print("Completed cycle block: " .. cycle .. " / " .. numberOfCycles)
     end
-    
-    -- Print when the entire large loop has finished its iteration
-    print("--- Finished massive cycle. Restarting immediately. ---") 
+    print("--- Finished massive cycle. Restarting immediately. ---")
 end
